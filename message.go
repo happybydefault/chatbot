a4b5ae5956ff6a757bc3863057fff798a0f6c1b3
Raúl Garrido <me@happybydefault.com>
2022-12-24 17:15:43 -0500

    Respond with a reply

--- message.go
+++ message.go
@@ -91,7 +91,12 @@ func (s *Server) handleMessage(ctx context.Context, message *events.Message) err
 	conversationResponse = strings.TrimSpace(conversationResponse)
 
 	response := &waProto.Message{
-		Conversation: proto.String(conversationResponse),
+		ExtendedTextMessage: &waProto.ExtendedTextMessage{
+			ContextInfo: &waProto.ContextInfo{
+				QuotedMessage: message.Message,
+			},
+			Text: proto.String(conversationResponse),
+		},
 	}
 
 	// Make sure there is a delay between receiving a message and sending a response,
