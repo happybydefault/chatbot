d14acfe9b29ce7616058573a30496fd1afb8e1b1
Raúl Garrido <me@happybydefault.com>
2023-01-02 17:07:47 -0500

    Add TODO comment

--- message.go
+++ message.go
@@ -14,6 +14,7 @@ import (
 	"github.com/happybydefault/chatbot/data"
 )
 
+// TODO: Make this concurrent for multiple chats.
 func (c *Client) handleMessage(message *events.Message) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
