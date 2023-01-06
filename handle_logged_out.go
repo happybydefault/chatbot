ea145a6b651f9eef77647f0335f62cf07c9e566b
Raúl Garrido <me@happybydefault.com>
2023-01-06 07:08:41 -0500

    Refactor `handleLoggedOutEvent`

--- handle_logged_out.go
+++ handle_logged_out.go
@@ -8,16 +8,17 @@ import (
 
 // TODO: Revisit logic.
 func (c *Client) handleLoggedOutEvent(loggedOut *events.LoggedOut) error {
-	if loggedOut.OnConnect {
-		if loggedOut.Reason.IsLoggedOut() {
-			err := c.whatsmeowClient.Store.Delete()
-			if err != nil {
-				return fmt.Errorf("failed to delete store: %w", err)
-			}
-		}
-
-		return fmt.Errorf("whatsmeow client was logged out because it failed to connect to WhatsApp")
+	if !loggedOut.OnConnect {
+		c.logger.Debug("whatsmeow client was logged out because of a stream:error message")
+		return nil
 	}
 
-	return nil
+	if loggedOut.Reason.IsLoggedOut() {
+		err := c.whatsmeowClient.Store.Delete()
+		if err != nil {
+			return fmt.Errorf("failed to delete store: %w", err)
+		}
+	}
+
+	return fmt.Errorf("whatsmeow client was logged out because it failed to connect to WhatsApp")
 }
